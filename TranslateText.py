e5b2d200d217409e5d347351aff600b1bd009c8b
Andrei Leca <andrei523070@gmail.com>
2022-02-23 09:26:23 +0100

    updated script

--- TranslateText.py
+++ TranslateText.py
@@ -2,20 +2,9 @@ import asyncio
 import os
 import argparse
 from pprint import pprint
+import sys
 
 from deep_translator import GoogleTranslator
-
-def remove_old_translated():
-    mypath =  os.path.dirname(os.path.abspath(__file__))
-    for f in os.listdir(mypath):
-        if f.startswith("translated"):
-            os.remove(f)
-            
-def remove_old_temps():
-    mypath =  os.path.dirname(os.path.abspath(__file__))
-    for f in os.listdir(mypath):
-        if f.startswith("temp"):
-            os.remove(f)
 
 class Translation:
     '''The script will translate the text file from one language to the next language.
@@ -26,13 +15,11 @@ class Translation:
     Example of language codes: 'fr' = French; 'sq' = Albanian; 'en' = English
     '''
     def __init__(self, *a):
-        self.from_language = args.from_language
-        self.to_language = args.to_language
+        self.from_language = args['s']
+        self.to_language = args['d']
         self.extract_file = extract_file
         self.translate_file = translate_file
-        self.transformed_file = transformed_file
-        with open(self.extract_file, 'a') as f:
-            f.write('\n')
+        self.rows = list( open(extract_file, "r").read().splitlines())
 
     def clean_text(self):
         text = open(self.extract_file, 'r').read()
@@ -41,18 +28,16 @@ class Translation:
         text = text.replace('.', '.\n')
         text = text.replace('!', '!\n')
         text = text.replace('?', '?\n')
+        self.rows = list(text.splitlines())
 
-        self.transformed_file = list(text.splitlines())
-
-        #f = open(self.transformed_file, 'w')
-        #f.write(text)
-    
+    ## Translite line
     async def translate_line(self, line):
         if( len(line.strip()) > 0 ):
             return GoogleTranslator(source=self.from_language, target=self.to_language).translate(line.strip())
         else:
             return ""
 
+    ## Requests
     async def requests_six(self, one, tow, three, four, five, six):
         return await asyncio.gather(
             self.translate_line(one),
@@ -62,7 +47,6 @@ class Translation:
             self.translate_line(five),
             self.translate_line(six)
         )
-        
     async def requests_three(self, one, tow, three):
         return await asyncio.gather(
             self.translate_line(one),
@@ -72,70 +56,97 @@ class Translation:
     async def requests_one(self, one):
         return await asyncio.gather( self.translate_line(one) )
 
+    ## Main Requests
     def write_translation(self):
-
-        rows =  self.transformed_file
         translated = []
+        rows_length = len(self.rows) - 1
+        rows_ter, columns_ter = os.popen('stty size', 'r').read().split()
 
-        #for i, line in enumerate(rows):
-        #    translation = self.translate_line(line)
-        #    print('\r\rTranslation progress [%d / %d]'%(i, len(rows)), end="")
-        #    translated.append(translation)
-        #lis = [1, 2, 3, 4, 5]
-
-
-        
-        
-        rows_length = len(rows) - 1
+        ## Async requests
         i = 0
         while(i <= rows_length):
-
             if( (rows_length - i) >= 6 ):
                 translated += asyncio.run(self.requests_six(
-                    rows[i].strip(), 
-                    rows[i+1].strip(), 
-                    rows[i+2].strip(),
-                    rows[i+3].strip(), 
-                    rows[i+4].strip(), 
-                    rows[i+5].strip()
+                    self.rows[i].strip(), 
+                    self.rows[i+1].strip(), 
+                    self.rows[i+2].strip(),
+                    self.rows[i+3].strip(), 
+                    self.rows[i+4].strip(), 
+                    self.rows[i+5].strip()
                 ))
                 i += 6
             else:
                 if( (rows_length - i) >= 3 ):
-                    translated += asyncio.run( self.requests_three(rows[i].strip(), rows[i+1].strip(), rows[i+2].strip()) )
+                    translated += asyncio.run( self.requests_three(self.rows[i].strip(), self.rows[i+1].strip(), self.rows[i+2].strip()) )
                     i += 3
                 else:
-                    translated += asyncio.run( self.requests_one(rows[i].strip()) )
+                    translated += asyncio.run( self.requests_one(self.rows[i].strip()) )
                     i += 1
 
-            # print( "\n%s \n%s \n%s" %(rows[i], rows[i+1], rows[i+2]) )
-            # print( "\n" )
+            ## Print terminal progress
+            title = "Translation progress"
+            end_row = '[%d / %d]'%(i, len(self.rows))
+            n_spaces = int(columns_ter)-len(title)-len(end_row)-2
 
-            print( '\r\rTranslation progress [%d / %d]'%(i, len(rows)), end="" )
-        
-        print("\n")
-        
+            spaces = ""
+            for h in range(0, n_spaces):
+                spaces += " "
+
+            print( '\r%s %s %s'%(title, spaces, end_row), end="" )
+
+
+        ## Write output
+        print("\nWritting file")
         with open(self.translate_file, 'w') as f:
             for i in translated:
                 f.write( str(i)+ '\n')
         f.close()
 
-
-
 if __name__ == "__main__":
-    remove_old_translated()
-    mypath =  os.path.dirname(os.path.abspath(__file__))
-    for f in os.listdir(mypath):
-        if f.endswith(".txt"):
-            argparser = argparse.ArgumentParser()
-            argparser.add_argument('from_language',help = 'Example of command to run: python TranslateText.py fr en')
-            argparser.add_argument('to_language',help = 'Example of command to run: python TranslateText.py fr en')
-            args = argparser.parse_args()
-            extract_file = "{}.txt".format(os.path.splitext(f)[0])
-            transformed_file = "temp_{}.txt".format(os.path.splitext(f)[0])
-            translate_file = "translated_{}_{}_{}.txt".format(args.from_language, args.to_language, os.path.splitext(f)[0])
-            t=Translation()
-            t.clean_text()
-            t.write_translation()
-            remove_old_temps()
-            print('translation complete - check text file that starts with translated')
+
+    print("       ....                                                                                                          ")
+    print("   .xH888888Hx.                                                                                                      ")
+    print(" .H8888888888888:                           .d``                                                                     ")
+    print(" 888*\"\"\"?\"\"*88888X        .u         .u     @8Ne.   .u                                                               ")
+    print("'f     d8x.   ^%88k    ud8888.    ud8888.   %8888:u@88N                                                              ")
+    print("'>    <88888X   '?8  :888'8888. :888'8888.   `888I  888.                                                             ")
+    print(" `:..:`888888>    8> d888 '88%\" d888 '88%\"    888I  888I                                                             ")
+    print("        `\"*88     X  8888.+\"    8888.+\"       888I  888I                                                             ")
+    print("   .xHHhx..\"      !  8888L      8888L       uW888L  888'                                                             ")
+    print("  X88888888hx. ..!   '8888c. .+ '8888c. .+ '*88888Nu88P                                                              ")
+    print(" !   \"*888888888\"     \"88888%    \"88888%   ~ '88888F`                                                                ")
+    print("        ^\"***\"`         \"YP'       \"YP'       888 ^                                                                  ")
+    print("     s                                        *8E   .x+=:.         ..                  s                             ")
+    print("    :8                                        '8>  z`    ^%  x .d88\"                  :8                            ")
+    print("   .88       .u    .                  u.    u. \"  z` .   <k  5888R                  .88           u.      .u    .   ")
+    print("  :888ooo  .d88B :@8c        u      x@88k u@88c.    .@8Ned8\"  '888R         u       :888ooo  ...ue888b   .d88B :@8c  ")
+    print("-*8888888 =\"8888f8888r    us888u.  ^\"8888\"\"8888\"  .@^%8888\"    888R      us888u.  -*8888888  888R Y888r =\"8888f8888r ")
+    print("  8888      4888>'88\"  .@88 \"8888\"   8888  888R  x88:  `)8b.   888R   .@88 \"8888\"   8888     888R I888>   4888>'88\"  ")
+    print("  8888      4888> '    9888  9888    8888  888R  8888N=*8888   888R   9888  9888    8888     888R I888>   4888> '    ")
+    print("  8888      4888>      9888  9888    8888  888R   %8\"    R88   888R   9888  9888    8888     888R I888>   4888>      ")
+    print(" .8888Lu=  .d888L .+   9888  9888    8888  888R    @8Wou 9%    888R   9888  9888   .8888Lu= u8888cJ888   .d888L .+   ")
+    print(" ^%888*    ^\"8888*\"    9888  9888   \"*88*\" 8888\" .888888P`    .888B . 9888  9888   ^%888*    \"*888*P\"    ^\"8888*\"    ")
+    print("   'Y\"        \"Y\"      \"888*\"\"888\"    \"\"   'Y\"   `   ^\"F      ^*888%  \"888*\"\"888\"    'Y\"       'Y\"          \"Y\"      ")
+    print("                        ^Y\"   ^Y'                               \"%     ^Y\"   ^Y'                                     ")
+    print("                                                                                                              ")
+
+
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('-s', help = 'Source language for translate file')
+    argparser.add_argument('-d', help = 'Destination language to saving file')
+    argparser.add_argument('-t', help = 'Source file for translation')
+    argparser.add_argument('-o', help = 'Output file translated')
+    argparser.add_argument('--compact', nargs='?', default="false", const="true", help = 'Compact text for reduce number of requests for most big files' )
+
+    args = vars(argparser.parse_args())
+
+    extract_file = "{}".format(args['t'])
+    translate_file = "translated_{}_{}_{}.txt".format(args['s'], args['d'], os.path.splitext(args['t'])[0])
+
+    if( args['o'] != None ) :
+        translate_file = args['o']
+
+    t=Translation()
+    if( args['compact'] == "true" ) :
+        t.clean_text()
+    t.write_translation()
